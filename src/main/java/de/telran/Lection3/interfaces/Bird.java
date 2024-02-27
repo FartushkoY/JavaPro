41bffb95d6b1b9186ee0f974889929f50683bbbb
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-02-28 00:23:47 +0100

    add

---
+++ src/main/java/de/telran/Lection3/interfaces/Bird.java
@@ -0,0 +1,12 @@
+package de.telran.Lection3.interfaces;
+
+public class Bird extends Introducable {
+
+    @Override
+    public  void introduce() {
+        System.out.println("Hello! I'm birrd!");
+    }
+
+//    public abstract void introduce();
+
+}
